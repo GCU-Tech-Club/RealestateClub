a43605753bfbcc9c31c50452a95fbc9935ec8245
ItsRiprod <riprodgames@gmail.com>
2024-10-08 17:50:59 -0700

    Tailwindcss + Template App

--- Realestateclub/src/App.tsx
+++ Realestateclub/src/App.tsx
@@ -4,31 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-black w-screen h-screen flex flex-col items-center justify-center">
+      <h1 className="text-white text-4xl">
+        REALESTATECLUB
+      </h1>
+      <p className="italic text-white">Template App</p>
+    </div>
   )
 }
 
--- Realestateclub/tailwind.config.js
+++ Realestateclub/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
