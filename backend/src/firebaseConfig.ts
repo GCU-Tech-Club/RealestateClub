8a9e68ec34e842fc0e944633f283e58bdc528c14
Tyler Friesen <tylerfriesen@Tylers-MacBook-Pro.local>
2024-10-18 21:09:54 -0700

    set up firebase project in backend, may need to move firebaseConfig.ts to frontend later

---
+++ backend/src/firebaseConfig.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const store = getFirestore(app);
+
+// Use the Firebase emulator locally
+if (process.env.NODE_ENV === 'development') {
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectFirestoreEmulator(store, 'http://localhost', 8080);
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import admin from 'firebase-admin';
+var serviceAccount = require("/app/firebase-sak.json");
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 
 const app = express();
 const port = 5001;
@@ -6,6 +13,26 @@ const port = 5001;
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+// Test route to check Firebase connection
+app.get('/firebase-test', async (req: Request, res: Response) => {
+    try {
+      // Check Firebase Admin connection by listing Firestore collections
+      const firestore = admin.firestore();
+      const collections = await firestore.listCollections();
+      
+      const collectionNames = collections.map(col => col.id);
+      res.status(200).json({
+        message: 'Firebase connected successfully!',
+        collections: collectionNames,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Failed to connect to Firebase',
+        error: error,
+      });
+    }
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
