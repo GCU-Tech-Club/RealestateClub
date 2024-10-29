b7978d1e4979189ae86e604d521c12ed198d8173
Tyler Friesen <tylerfriesen@Tylers-MacBook-Pro.local>
2024-10-28 23:21:16 -0700

    now firebase project works correctly for prod, test, and dev environments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,36 @@ import express, { Request, Response } from 'express';
 import admin from 'firebase-admin';
 //var serviceAccount = require("../firebase-sak.json");
 require('dotenv').config()
-const serviceAccount = JSON.parse(process.env.FIREBASE_SAK as string);
+const productionMode = process.env.PRODUCTION_MODE === 'true';
 
 // Initialize Firebase Admin SDK
-admin.initializeApp({
+if (productionMode) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SAK as string);
+
+  admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  
+  console.log('Using Live Firebase Data!');
+} else {
+  admin.initializeApp();
+  console.log("Using Firebase Emulator.");
+}
+
+const firestore = admin.firestore();
+const auth = admin.auth();
+
+if (!productionMode) {
+  firestore.settings({
+    host: 'localhost:7001',
+    projectId: 'gcurealestate-ae639',
+    ssl: false,
+  })
+
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:9099';
+  process.env.FIRESTORE_EMULATOR_HOST = 'localhost:7001';
+  console.log("Connected to Firestore and Auth emulators.");
+}
 
 const app = express();
 const port = 5001;
@@ -20,7 +44,6 @@ app.get('/', (req, res) => {
 app.get('/firebase-test', async (req: Request, res: Response) => {
     try {
       // Check Firebase Admin connection by listing Firestore collections
-      const firestore = admin.firestore();
       const collections = await firestore.listCollections();
       
       const collectionNames = collections.map(col => col.id);
