0c94f3442b9f548321280f0194830c4f9efec23d
Blake0206 <blakealmanza02@gmail.com>
2024-11-03 19:45:45 -0700

    removed test comment

--- backend/src/routes/api/v1/user/userFetch.ts
+++ backend/src/routes/api/v1/user/userFetch.ts
@@ -3,14 +3,6 @@ import { admin, firestore } from '../../../..';
 
 const router = Router();
 
-/*
-router.get('/', (req, res) => {
-  res.status(200).json({
-    message: "User has been fetched",
-    header: req.headers.authorization,
-  });
-})
-*/
 
 router.get('/', async (req: Request, res: Response): Promise<void> => {
   try {
