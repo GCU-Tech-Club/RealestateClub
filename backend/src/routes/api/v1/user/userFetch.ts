4fe13db40207b303fc1d683a2ba0dd3d05863c74
Blake0206 <blakealmanza02@gmail.com>
2024-11-03 19:11:16 -0700

    Testing - Removed get firestore data

--- backend/src/routes/api/v1/user/userFetch.ts
+++ backend/src/routes/api/v1/user/userFetch.ts
@@ -3,37 +3,8 @@ import { admin, firestore } from '../../../..';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response): Promise<void> => {
-  try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      res.status(401).json({ message: 'Unauthorized'});
-      return;
-    }
-
-    const idToken = authHeader.split('Bearer ')[1];
-
-    //const decodedToken = await admin.auth().verifyIdToken(idToken);  // If passing a real auth token, this will work
-    //const uid = decodedToken.uid;                                    // For testing purposes, only expecting uid
-    const uid = idToken; // For testing only
-
-    const userDoc = await firestore.collection('User').doc(uid).collection('UserData').doc(uid).get()
-
-    if (!userDoc.exists) {
-      res.status(404).json({ message: 'User data not found' });
-      return;
-    }
-
-    const userData = { id: userDoc.id, ...userDoc.data() };
-    
-    res.status(200).json({
-      userData: userData,
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error,
-    });
-  }
-});
+router.get('/', (req, res) => {
+  res.send("User has been fetched.")
+})
 
 export default router;
