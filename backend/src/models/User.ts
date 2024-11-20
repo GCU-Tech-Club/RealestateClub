1ac30791167850d147c19412ac8e64fc9b0cf15e
Andrew Forster <forsterandrew15@gmail.com>
2024-11-19 18:43:21 -0700

    Made a User Interface / Model for creating new users.

---
+++ backend/src/models/User.ts
@@ -0,0 +1,7 @@
+export default interface User {
+    uid: string;
+    name: string;
+    bio: string;
+    image: string;
+    major: string;
+    }
--- backend/src/routes/api/v1/user/userCreate.ts
+++ backend/src/routes/api/v1/user/userCreate.ts
@@ -1,20 +1,54 @@
 import { Router } from 'express';
-// import User from '../models/User';
+import User from '../../../../models/User';
+import { firestore } from '../../../..';
+
 const router = Router();
 
 router.post('/', async (req, res) => {
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     gradYear: req.body.gradYear,
-    // });
 
-    // try{
-    //     const newUser = await user.save();
-    //     res.status(201).json(newUser);
-    // }catch {
-    //     res.status(400)
-    // }
+    const userData: User = req.body;
+    const userRef = firestore.collection('Users').doc(userData.uid);
+
+    if (userData.uid === undefined) {
+        res.status(400).json({
+            message: 'User ID is required',
+
+        });
+        return;
+    }
+
+    if (userData.name === undefined) {
+        res.status(400).json({
+            message: 'Name is required',
+        });
+        return;
+    }
+
+    if (userData.bio === undefined) {
+        res.status(400).json({
+            message: 'Bio is required',
+        });
+        return;
+    }
+
+    if (userData.image === undefined) {
+        res.status(400).json({
+            message: 'Image is required',
+        });
+        return;
+    }
+
+    if (userData.major === undefined) {
+        res.status(400).json({
+            message: 'Major is required',
+        });
+        return;
+    }
+
+    await userRef.set(userData, { merge: true });
+    res.status(201).json({
+        message: 'User created successfully',
+    });
 });
 
 export default router;
