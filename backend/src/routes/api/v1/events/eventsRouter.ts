51dde170db51973e74ba0ab9ce75ea9a6db00db8
Blake0206 <blakealmanza02@gmail.com>
2024-11-19 17:47:17 -0700

    Added get event

--- backend/src/routes/api/v1/events/eventsRouter.ts
+++ backend/src/routes/api/v1/events/eventsRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import getAllEvents from './getAllEvents';
+import getEvent from './getEvent';
 
 const router = Router();
 
 router.use('/', getAllEvents)
+router.use('/test', getEvent)
 
 export default router
---
+++ backend/src/routes/api/v1/events/getEvent.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+import { firestore } from '../../../..';
+
+const router = Router();
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const eventDoc = await firestore.collection('Events').doc(id).get();
+
+        if (!eventDoc.exists) {
+            res.status(400).json({message: 'Event data not found'});
+        };
+
+        const eventData = { ...eventDoc.data() };
+
+        res.status(200).json({
+            eventData: eventData
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+});
+
+export default router;
