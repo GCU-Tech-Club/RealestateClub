a15c17d6e3a41a6df01e602f97f0569516ab8b29
Blake0206 <blakealmanza02@gmail.com>
2025-01-14 18:55:35 -0700

    Another test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,8 @@ var networkInterfaces = os.networkInterfaces();
 require('dotenv').config()
 const productionMode = process.env.PRODUCTION_MODE === 'true';
 
-const testServerIP = String(networkInterfaces['en0'][1].address);
+const testServerIP = String(networkInterfaces['en0'][1].address) || '172.31.29.127';
+console.log(testServerIP);
 const localServerIP = '127.0.0.1'; // External IP: 172.24.250.123
 
 const ipAddress = productionMode ? testServerIP : localServerIP;
