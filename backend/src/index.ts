318114f4890f9bcc6517997e5618c08da1ac75a3
Blake0206 <blakealmanza02@gmail.com>
2025-01-28 21:40:06 -0700

    Trying axios

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,23 +12,32 @@ const port = 5001;
 
 app.use(cors());
 
-var os = require('os');
-function getIpAddress(internal: boolean = false): string {
-  const networkInterfaces = os.networkInterfaces();
+import axios from 'axios';
 
-  // const ipAddresses: string[] = [];
+async function getInternalIp() {
+  if (process.env.DEPLOYMENT_ENV !== 'test') {
+    console.log("Skipping internal IP retrieval (not in dev mode)");
+    return null;
+  }
+  try {
+      const response = await axios.get('http://169.254.169.254/latest/meta-data/local-ipv4', {
+          timeout: 5000, // Set a timeout to prevent long delays
+      });
 
-  // for (const interfaceName in networkInterfaces) {
-  //   for (const info of networkInterfaces[interfaceName]) {
-  //     if (info.internal === internal && info.family === 'IPv4') {
-  //       //return info.address;
-  //       ipAddresses.push(info.address);
-  //     }
-  //   }
-  // }
-  return networkInterfaces.lo0[0].address;
-  //throw new Error(`No ${internal ? 'internal' : 'external'} IP address found`);
+      return response.data;
+  } catch (error) {
+      console.error("Error fetching internal IP:", error instanceof Error ? error.message : error);
+      return null;
+  }
 }
+
+let testServerIP = '';
+getInternalIp().then(internalIp => {
+  const testServerIP = internalIp;
+  console.log("Test server IP:", testServerIP);
+});
+
+
 
 let testDevelopment: boolean;
 if (process.env.DEPLOYMENT_ENV === 'test') {
@@ -37,27 +46,25 @@ if (process.env.DEPLOYMENT_ENV === 'test') {
   testDevelopment = false;
 }
 
+
+let ipAddress = '';
+
 console.log(`Deployment Environment: ${process.env.DEPLOYMENT_ENV}`);
 console.log(`Test Development: ${testDevelopment}`);
 
-let testServerIP: string | null;
-let localServerIP: string | null;
-let ipAddress: string | null;
-
 try {
-  //console.log(getIpAddress(true));
   //testServerIP = getIpAddress(true); // old = false     172.31.29.127 
 
-  // localServerIP = os.networkInterfaces().lo0[0].address;
-  // testServerIP = os.networkInterfaces().eth0[0].address;
-  // ipAddress = testDevelopment ? testServerIP : localServerIP;
- 
-  ipAddress = testDevelopment ? os.networkInterfaces().eth0 : os.networkInterfaces().lo0[0].address;
+  ipAddress = testDevelopment ? testServerIP : '127.0.0.1';
   console.log(ipAddress);
 
 } catch (error) {
   throw new Error(`Failed to get IP address: ${error}`);
 }
+
+
+
+
 
 // Initialize Firebase Admin SDK
 if (productionMode) {
@@ -73,6 +80,7 @@ if (productionMode) {
   admin.initializeApp();
   console.log("Using Firebase Emulator.");
 }
+
 
 const firestore = admin.firestore();
 const auth = admin.auth();
